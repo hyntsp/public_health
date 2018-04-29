00c5e5e159005f7ca4316a9fc1085e1a75221add
sx <599133798@qq.com>
2018-04-29 15:18:34 +0800

    --

--- app/Http/Requests/StoreCheckup.php
+++ app/Http/Requests/StoreCheckup.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreHeathArchive extends FormRequest
+class StoreCheckup extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
